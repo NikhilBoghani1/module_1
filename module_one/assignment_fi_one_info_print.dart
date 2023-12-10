83610b3496655272a341c5b5c9a011e0fdf54132
Nikhil Boghani <131741338+NikhilBoghani1@users.noreply.github.com>
2023-12-10 18:20:05 +0530

    Update assignment_fi_one_info_print.dart

--- module_one/assignment_fi_one_info_print.dart
+++ module_one/assignment_fi_one_info_print.dart
@@ -1,7 +1,7 @@
 void main(){
   print('* * * * * * * * * * * * * * * * * * * * * * * * * * *');
   print('*            My Name is Nikhil Boghani             *');
-  print('*        My Birthdate is 7th June 1996          *');
+  print('*        My Birthdate is 7th June 2004          *');
   print('*               I am 20 years Old.                  *');
   print('* My address is Palanpur Patiya, Rander road, Surat *');
   print('* * * * * * * * * * * * * * * * * * * * * * * * * * *');
